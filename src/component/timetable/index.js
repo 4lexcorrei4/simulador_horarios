8eaf030ab3a4851dbab0c5d68074ac22d1f41ad0
Alexandre Correia <at.correia@campus.fct.unl.pt>
2021-09-14 11:44:31 +0100

    Reformated update time hour

--- src/component/timetable/index.js
+++ src/component/timetable/index.js
@@ -102,7 +102,7 @@ const Timetable = ({maxCellShifts, hours, cellShifts, cellFilling, chosenShifts,
             </div>
         </div>
         <span id="update-time">
-            Última atualização: {update_time ? update_time_date.getDate() + "/" + (update_time_date.getMonth() + 1) + "/" + update_time_date.getFullYear() : ""}
+            Última atualização: {update_time ? update_time_date.getDate() + "/" + (update_time_date.getMonth() + 1) + "/" + update_time_date.getFullYear() + ", " + (update_time_date.getHours() < 10 ? "0" + update_time_date.getHours() : update_time_date.getHours()) + ":" + (update_time_date.getMinutes() < 10 ? "0" + update_time_date.getMinutes() : update_time_date.getMinutes()) : ""}
         </span>
     </>
 };
