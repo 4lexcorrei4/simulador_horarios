7bf7f5bf434948427951c02585a275de8ef262ec
Alexandre Correia <at.correia@campus.fct.unl.pt>
2021-09-14 13:11:46 +0100

    Updated shift types

--- src/component/timetable/index.js
+++ src/component/timetable/index.js
@@ -81,7 +81,7 @@ const Timetable = ({maxCellShifts, hours, cellShifts, cellFilling, chosenShifts,
                                                                            onClick={() => saveOrUnsaveShift(shift_info.subject.id, shift_info.type.name, shift_info.number)}>&#9733;</div>
                                                         }
                                                         <h3><a href={shift_info.subject.url ? shift_info.subject.url : "javascript:;"} target={shift_info.subject.url ? "_blank" : ""} rel="noreferrer"><span title={shift_info.subject.name}>{shift_info.subject.short}</span></a></h3>
-                                                        <p><a href={shift_info.url ? shift_info.url : "javascript:;"} target={shift_info.url ? "_blank" : ""} rel="noreferrer"><span title={shift_info.type.title + " " + shift_info.number}>{shift_info.type.name.toUpperCase()} {shift_info.number}</span></a><br />{shift_info.building ? shift_info.building + ": " + shift_info.room : ""}</p>
+                                                        <p><a href={shift_info.url ? shift_info.url : "javascript:;"} target={shift_info.url ? "_blank" : ""} rel="noreferrer"><span title={shift_info.type.title + " " + shift_info.number}>{shift_info.type.name.toUpperCase()} {shift_info.number}</span></a><br />{shift_info.building ? shift_info.building + ": " + shift_info.room : "Online"}</p>
                                                     </td>
                                                 )
                                                 : <ElementTD times={maxCellShifts[day] - cellFilling[hours.indexOf(hour)][day]} />
